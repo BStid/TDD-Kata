ce3eda8cf35a706a569c104c791448df6ca6d901
Ben Stidham <benjamindstidham@gmail.com>
2019-02-03 16:54:27 -0600

    working TDD code but needs refactoring

--- spec/PrimeFactors.spec.js
+++ spec/PrimeFactors.spec.js
@@ -4,5 +4,12 @@ describe("PrimeFactors", function() {
 
     expect(primeFactors.of(1)).toEqual([]);
     expect(primeFactors.of(2)).toEqual([2]);
+    expect(primeFactors.of(3)).toEqual([3]);
+    expect(primeFactors.of(4)).toEqual([2, 2]);
+    expect(primeFactors.of(5)).toEqual([5]);
+    expect(primeFactors.of(6)).toEqual([2, 3]);
+    expect(primeFactors.of(7)).toEqual([7]);
+    expect(primeFactors.of(8)).toEqual([2, 2, 2]);
+    expect(primeFactors.of(9)).toEqual([3, 3]);
   });
 });
--- src/PrimeFactors.js
+++ src/PrimeFactors.js
@@ -1,6 +1,20 @@
-let PrimeFactors = function() {};
+let PrimeFactors = function() {
+  return;
+};
 
-PrimeFactors.prototype.of = function() {
-  return [];
+PrimeFactors.prototype.of = function(n) {
+  let result = [];
+
+  while (n > 1) {
+    let divisor = 2;
+    while (divisor <= n) {
+      while (0 === n % divisor) {
+        result.push(divisor);
+        n /= divisor;
+      }
+      ++divisor;
+    }
+  }
+  return result;
 };
 module.exports = new PrimeFactors();
