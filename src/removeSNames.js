5d928181392a7594ee53b06ffda955d8ba517b85
Ben Stidham <benjamindstidham@gmail.com>
2019-02-04 02:12:33 -0600

    refactored code

--- src/removeSNames.js
+++ src/removeSNames.js
@@ -1,12 +1,5 @@
 const removeSNames = names => {
-  let newNames = names;
-  names.forEach(name => {
-    if (name.toLowerCase().charAt(0) === "s") {
-      const index = newNames.indexOf(name);
-      newNames.splice(index, 1);
-    }
-  });
-  return newNames;
+  return names.filter(name => name.toLowerCase().charAt(0) !== "s");
 };
 
 module.exports = removeSNames;
