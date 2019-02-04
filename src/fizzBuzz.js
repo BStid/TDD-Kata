9892a181ac540df5151546dbbeb4a46e2744b3a6
Ben Stidham <benjamindstidham@gmail.com>
2019-02-04 14:47:27 -0600

    refactored code

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,13 +1,20 @@
 const fizzBuzz = number => {
-  if (number % 2 === 0) {
-    return number % 3 === 0 ? "fizz buzz" : "fizz";
-  } else if (number % 3 === 0) {
-    return "buzz";
-  } else if (number % 2 === 0 && number % 3 === 0) {
-    return "fizz buzz";
-  } else {
-    return "";
-  }
+  //   if (number % 2 === 0) {
+  //     return number % 3 === 0 ? "fizz buzz" : "fizz";
+  //   } else if (number % 3 === 0) {
+  //     return "buzz";
+  //   } else {
+  //     return "";
+  //   }
+
+  //Refactored Code. Clean, but harder to understand quickly. Look into other methods.
+  return number % 2 === 0
+    ? number % 3 === 0
+      ? "fizz buzz"
+      : "fizz"
+    : number % 3 === 0
+    ? "buzz"
+    : "";
 };
 
 module.exports = fizzBuzz;
