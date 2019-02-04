90ce048aeec1501b428e2556de3ba12e4d548af0
Ben Stidham <benjamindstidham@gmail.com>
2019-02-04 14:53:54 -0600

    another method added

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -8,13 +8,23 @@ const fizzBuzz = number => {
   //   }
 
   //Refactored Code. Clean, but harder to understand quickly. Look into other methods.
-  return number % 2 === 0
-    ? number % 3 === 0
-      ? "fizz buzz"
-      : "fizz"
-    : number % 3 === 0
-    ? "buzz"
-    : "";
+  //   return number % 2 === 0
+  //     ? number % 3 === 0
+  //       ? "fizz buzz"
+  //       : "fizz"
+  //     : number % 3 === 0
+  //     ? "buzz"
+  //     : "";
+
+  //Another Refactored Method.
+  let words = [];
+  if (number % 2 === 0) {
+    words.push("fizz");
+  }
+  if (number % 3 === 0) {
+    words.push("buzz");
+  }
+  return words.join(" ");
 };
 
 module.exports = fizzBuzz;
