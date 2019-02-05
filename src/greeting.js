aae869de82979abc7e4af3dba69542d2f84c874b
Ben Stidham <benjamindstidham@gmail.com>
2019-02-05 12:38:44 -0600

    accounting for all uppercase, needs refactoring

--- src/greeting.js
+++ src/greeting.js
@@ -1,7 +1,19 @@
 const greeting = name => {
-  return typeof name === "string"
-    ? `Hello, ${name.charAt(0).toUpperCase() + name.slice(1)}.`
-    : "Hello, my friend.";
+  let letter = 0;
+  if (typeof name === "string") {
+    while (name.charAt(letter) === name.charAt(letter).toUpperCase()) {
+      if (letter === name.length) {
+        return `HELLO, ${name}!`;
+      }
+      letter++;
+    }
+    return `Hello, ${name.charAt(0).toUpperCase() + name.slice(1)}.`;
+  }
+  return "Hello, my friend.";
 };
 
 module.exports = greeting;
+
+// return typeof name === "string"
+//     ? `Hello, ${name.charAt(0).toUpperCase() + name.slice(1)}.`
+//     : "Hello, my friend.";
--- src/spec/greeting.spec.js
+++ src/spec/greeting.spec.js
@@ -5,14 +5,21 @@ describe("Give a greeting", () => {
     expect(greeting("Ben")).toMatch("Ben");
     expect(greeting("Ben")).toBe("Hello, Ben.");
   });
+
   it("should account for lowercase names", () => {
     expect(greeting("sarah")).toMatch("Sarah");
     expect(greeting("ben")).toMatch("Ben");
     expect(greeting("mike")).toBe("Hello, Mike.");
   });
+
+  it("should account for all caps", () => {
+    expect(greeting("BEN")).toBe("HELLO, BEN!");
+  });
+
   it("should account for null inputs", () => {
     expect(greeting()).toBe("Hello, my friend.");
   });
+
   it("should account for number inputs", () => {
     expect(greeting(123)).toBe("Hello, my friend.");
   });
