40fe424921a52f504c429f7e082e7ca17cd15c91
Ben Stidham <benjamindstidham@gmail.com>
2019-02-05 12:58:53 -0600

    refactored code, might could do it even more...

--- src/greeting.js
+++ src/greeting.js
@@ -1,19 +1,19 @@
 const greeting = name => {
-  let letter = 0;
-  if (typeof name === "string") {
-    while (name.charAt(letter) === name.charAt(letter).toUpperCase()) {
-      if (letter === name.length) {
-        return `HELLO, ${name}!`;
-      }
-      letter++;
-    }
-    return `Hello, ${name.charAt(0).toUpperCase() + name.slice(1)}.`;
-  }
-  return "Hello, my friend.";
+  return typeof name === "string" ? checkAllCaps(name) : "Hello, my friend.";
 };
 
+const checkAllCaps = name => {
+  let letter = 0;
+  while (name.charAt(letter) === name.charAt(letter).toUpperCase()) {
+    if (letter === name.length) {
+      return `HELLO, ${name}!`;
+    }
+    letter++;
+  }
+  return `Hello, ${name.charAt(0).toUpperCase() + name.slice(1)}.`;
+};
 module.exports = greeting;
 
-// return typeof name === "string"
+//   return typeof name === "string"
 //     ? `Hello, ${name.charAt(0).toUpperCase() + name.slice(1)}.`
 //     : "Hello, my friend.";
