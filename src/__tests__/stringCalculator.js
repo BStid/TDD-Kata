70758a36ce5208f5a6f991df388d974d2b0305ce
Ben Stidham <benjamindstidham@gmail.com>
2019-02-04 22:48:08 -0600

    completed working code, needs refactoring

---
+++ src/__tests__/stringCalculator.js
@@ -0,0 +1,11 @@
+const stringCalculator = require("../stringCalculator");
+
+describe("String Calculator", () => {
+  it("should return added sum", () => {
+    expect(stringCalculator("2")).toEqual(2);
+    expect(stringCalculator("2", "4")).toEqual(6);
+  });
+  it("should account for nothing passed", () => {
+    expect(stringCalculator()).toBe("");
+  });
+});
---
+++ src/stringCalculator.js
@@ -0,0 +1,13 @@
+let stringCalculator = (string1, string2) => {
+  if (string1) {
+    let newNumber = parseInt(string1);
+    if (string2) {
+      let newNumber2 = parseInt(string2);
+      return newNumber + newNumber2;
+    }
+    return newNumber;
+  }
+  return "";
+};
+
+module.exports = stringCalculator;
