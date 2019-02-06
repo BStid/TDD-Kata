b54e4b946125b056c41f09ebe18d023500f05973
Ben Stidham <benjamindstidham@gmail.com>
2019-02-05 18:48:04 -0600

    refactored test code

--- src/spec/greeting.spec.js
+++ src/spec/greeting.spec.js
@@ -1,28 +1,39 @@
 const greeting = require("../greeting");
 
+let greetingToBe = (recieved, expected) => {
+  return expect(greeting(recieved)).toBe(expected);
+};
+let greetingToMatch = (recieved, expected) => {
+  return expect(greeting(recieved)).toMatch(expected);
+};
 describe("Give a greeting", () => {
   it("should interpolate a name into a greeting", () => {
-    expect(greeting("Ben")).toMatch("Ben");
-    expect(greeting("Ben")).toBe("Hello, Ben.");
+    greetingToMatch("Ben", "Ben");
+    greetingToBe("Ben", "Hello, Ben.");
   });
 
   it("should account for lowercase names", () => {
-    expect(greeting("sarah")).toMatch("Sarah");
-    expect(greeting("ben")).toMatch("Ben");
-    expect(greeting("mike")).toBe("Hello, Mike.");
+    greetingToMatch("sarah", "Sarah");
+    greetingToMatch("ben", "Ben");
+    greetingToBe("mike", "Hello, Mike.");
   });
 
   it("should account for all caps", () => {
-    expect(greeting("BEN")).toBe("HELLO, BEN!");
-    expect(greeting("BEn")).toBe("Hello, BEn.");
-    expect(greeting("BEn")).toMatch("BEn");
+    greetingToBe("BEN", "HELLO, BEN!");
+    greetingToBe("BEn", "Hello, BEn.");
+    greetingToMatch("BEn", "BEn");
   });
 
   it("should account for null inputs", () => {
     expect(greeting()).toBe("Hello, my friend.");
+    greetingToBe(null, "Hello, my friend.");
   });
 
   it("should account for number inputs", () => {
-    expect(greeting(123)).toBe("Hello, my friend.");
+    greetingToBe(123, "Hello, my friend.");
   });
+});
+
+describe("Multiple Names", () => {
+  it("should handle an array of names", () => {});
 });
